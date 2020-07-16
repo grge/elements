6f9dc792168083fde4ad91baf49f4ed0065a1595
George Dickeson <george.dickeson@gmail.com>
2020-07-17 08:23:29 +0930

    Add a p_reduce test

--- src/test/test_polynomial.ts
+++ src/test/test_polynomial.ts
@@ -1,6 +1,6 @@
 import test from 'ava';
 
-import { p_eq, p_add, p_norm } from '../polynomial'
+import { p_eq, p_add, p_norm, p_reduce } from '../polynomial'
 
 test('Zero polynomials are equal', t => {
     t.true(p_eq([], []));
@@ -42,4 +42,13 @@ test("Polynomial normalisation removes zero terms", t => {
         {m: [1, 0], coef: 1},
     ];
     t.deepEqual(p_norm(unnormed), normed)
+})
+
+test("Polynomial reduction. One variable, one divisor.", t=> {
+    // x^3 - 2x^2 - 4 = (x - 3) * (x^2 + x + 3) + 5
+    let divisor = [{m:[1], coef:1}, {m:[0], coef:-3}]
+    let dividend = [{m:[3], coef:1}, {m:[2], coef:-2}, {m:[0], coef:-4}]
+    let [Q, r] = p_reduce(dividend, [divisor], 'lex')
+    t.deepEqual(Q, [[{m:[2], coef:1}, {m:[1], coef:1}, {m:[0], coef:3}]])
+    t.deepEqual(r, [{m:[0], coef:5}])
 })
